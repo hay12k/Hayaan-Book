9be2e6e01c6a1776ad5e493a766296c3d11df9a1
hay12k <hayaangroup93@gmail.com>
2023-02-17 14:55:24 +0300

    modified:   assets/script.js 	modified:   assets/style.css 	modified:   index.html

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,11 @@
+searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+
 window.onscroll = () => {
+    searchForm.classList.remove('active');
     if(window.scrollY > 80){
         document.querySelector('.header .header-2').classList.add('active');
     }else{
