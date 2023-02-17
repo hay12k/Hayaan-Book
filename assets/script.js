e55ecc8a2fe9b2d718e9383dd79d0e776670e35d
hay12k <hayaangroup93@gmail.com>
2023-02-17 14:37:01 +0300

    modified:   assets/script.js 	modified:   assets/style.css 	modified:   index.html

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+window.onscroll = () => {
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+window.onload = () => {
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
