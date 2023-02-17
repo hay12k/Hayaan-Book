6d9ae80da4b6c86581f1ca794493b2982f846463
hay12k <hayaangroup93@gmail.com>
2023-02-17 16:44:23 +0300

    modified:   assets/script.js 	modified:   assets/style.css 	modified:   index.html

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,13 @@ document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
 }
 
+let loginForm = document.querySelector('.login-form-container');
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+document.querySelector('#clos-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
 
 window.onscroll = () => {
     searchForm.classList.remove('active');
